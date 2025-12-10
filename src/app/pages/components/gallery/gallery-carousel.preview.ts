15ef9ae91497597462cbc7ad2f62be6d05c4e8e5
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2025-12-10 23:55:56 +0100

    update images

--- src/app/pages/components/gallery/gallery-carousel.preview.ts
+++ src/app/pages/components/gallery/gallery-carousel.preview.ts
@@ -2,6 +2,7 @@ import { NgOptimizedImage } from '@angular/common';
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { ElbGalleryImports } from '@elbe/ui/gallery';
 import { HlmCarouselImports } from '@spartan-ng/helm/carousel';
+import { images } from './images';
 
 @Component({
   selector: 'elb-gallery-carousel-preview',
@@ -35,44 +36,5 @@ import { HlmCarouselImports } from '@spartan-ng/helm/carousel';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class GalleryCarouselPreview {
-  images: {
-    src: string;
-    width: number;
-    height: number;
-    alt: string;
-    previewUrl: string;
-    previewWidth: number;
-    previewHeight: number;
-  }[] = [
-    {
-      src: 'https://images.unsplash.com/photo-1623341214825-9f4f963727da?q=80&w=2370&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      alt: 'A bowl of ramen with chopsticks and a glass of beer',
-      width: 2370,
-      height: 1580,
-      previewUrl:
-        'https://images.unsplash.com/photo-1623341214825-9f4f963727da?q=50&w=800&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      previewWidth: 800,
-      previewHeight: 533,
-    },
-    {
-      src: 'https://images.unsplash.com/photo-1612927601601-6638404737ce?q=80&w=1287&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      alt: 'Noodles with sliced lemon on black ceramic bowl',
-      width: 1287,
-      height: 1931,
-      previewUrl:
-        'https://images.unsplash.com/photo-1612927601601-6638404737ce?q=80&w=800&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      previewWidth: 800,
-      previewHeight: 1200,
-    },
-    {
-      src: 'https://images.unsplash.com/photo-1611810174991-5cdd99a2c6b2?q=80&w=2670&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      alt: 'Green vegetable on white ceramic bowl',
-      width: 2670,
-      height: 1780,
-      previewUrl:
-        'https://images.unsplash.com/photo-1611810174991-5cdd99a2c6b2?q=80&w=800&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-      previewWidth: 800,
-      previewHeight: 533,
-    },
-  ];
+  images = images;
 }
---
+++ src/app/pages/components/gallery/images.ts
@@ -0,0 +1,41 @@
+export const images: {
+  src: string;
+  width: number;
+  height: number;
+  alt: string;
+  previewUrl: string;
+  previewWidth: number;
+  previewHeight: number;
+}[] = [
+  {
+    src: 'https://images.unsplash.com/photo-1598926737692-2dc95228f1ed?q=80&w=2532&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    // white-kayak-on-lake-near-green-trees-during-daytime
+    alt: 'White kayak on lake near green trees during daytime',
+    width: 2532,
+    height: 1424,
+    previewUrl:
+      'https://images.unsplash.com/photo-1598926737692-2dc95228f1ed?q=80&w=800&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    previewWidth: 800,
+    previewHeight: 450,
+  },
+  {
+    src: 'https://images.unsplash.com/photo-1515409116264-67afef5e27a7?q=80&w=1587&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    alt: 'Green vegetable on white ceramic bowl',
+    width: 1587,
+    height: 2380,
+    previewUrl:
+      'https://images.unsplash.com/photo-1515409116264-67afef5e27a7?q=80&w=800&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    previewWidth: 800,
+    previewHeight: 1200,
+  },
+  {
+    src: 'https://images.unsplash.com/photo-1681308065147-4c0a0777a969?q=80&w=2675&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    alt: 'Noodles with sliced lemon on black ceramic bowl',
+    width: 2675,
+    height: 1773,
+    previewUrl:
+      'https://images.unsplash.com/photo-1681308065147-4c0a0777a969?q=80&w=800&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    previewWidth: 800,
+    previewHeight: 530,
+  },
+];
