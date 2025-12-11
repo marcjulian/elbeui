4d013d985c8852aee1eec136bc88561d91462738
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2025-12-11 12:41:25 +0100

    add dark theme toggle

--- src/app/ui/header.ts
+++ src/app/ui/header.ts
@@ -1,14 +1,15 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, computed, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { NgIcon, provideIcons } from '@ng-icons/core';
-import { lucideGithub } from '@ng-icons/lucide';
+import { lucideGithub, lucideMoon, lucideSun } from '@ng-icons/lucide';
 import { HlmButtonImports } from '@spartan-ng/helm/button';
 import { config } from '../config';
+import { ThemeService } from './theme';
 
 @Component({
   selector: 'elb-header',
   imports: [RouterLink, NgIcon, HlmButtonImports],
-  providers: [provideIcons({ lucideGithub })],
+  providers: [provideIcons({ lucideGithub, lucideSun, lucideMoon })],
   template: `
     <header
       class="border-border bg-background/40 sticky top-0 z-10 flex h-(--header-height) items-center justify-between border-b px-4 backdrop-blur-lg"
@@ -19,9 +20,17 @@ import { config } from '../config';
         <a hlmBtn variant="ghost" href="${config.github}" target="_blank" rel="noopener noreferrer">
           <ng-icon name="lucideGithub" />
         </a>
+        <button hlmBtn variant="ghost" (click)="_themeService.toggle()">
+          <ng-icon [name]="_themeIcon()" />
+        </button>
       </nav>
     </header>
   `,
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class Header {}
+export class Header {
+  protected _themeService = inject(ThemeService);
+  protected _themeIcon = computed(() =>
+    this._themeService.theme() === 'dark' ? 'lucideMoon' : 'lucideSun',
+  );
+}
---
+++ src/app/ui/theme.ts
@@ -0,0 +1,35 @@
+import { afterNextRender, Injectable, signal } from '@angular/core';
+
+const Theme = ['dark', 'light'] as const;
+type Theme = (typeof Theme)[number];
+
+@Injectable({ providedIn: 'root' })
+export class ThemeService {
+  private _theme = signal<Theme | undefined>(undefined);
+  public readonly theme = this._theme.asReadonly();
+
+  constructor() {
+    afterNextRender(() => {
+      this.syncTheme();
+    });
+  }
+
+  private themePreference(): Theme {
+    const theme = localStorage.getItem('theme');
+    if (theme && Theme.includes(theme as Theme)) {
+      return theme as Theme;
+    }
+    return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+  }
+
+  private syncTheme() {
+    this._theme.set(this.themePreference());
+  }
+
+  public toggle() {
+    const newTheme = this._theme() === 'dark' ? 'light' : 'dark';
+    this._theme.set(newTheme);
+    document.documentElement.classList.toggle('dark', newTheme === 'dark');
+    localStorage.setItem('theme', newTheme);
+  }
+}
