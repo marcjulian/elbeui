0615b4ea4935d413117e960b907ecf32d57a79d4
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2025-12-11 21:57:17 +0100

    center preview, remove unused images

--- src/app/ui/preview.ts
+++ src/app/ui/preview.ts
@@ -3,7 +3,8 @@ import { Directive } from '@angular/core';
 @Directive({
   selector: '[elbPreview]',
   host: {
-    class: 'border-border flex items-center rounded-md border p-6 md:p-10 mt-4',
+    class:
+      'border-border flex items-center justify-center rounded-md border min-h-60 p-6 md:p-10 mt-4',
   },
 })
 export class Preview {}
