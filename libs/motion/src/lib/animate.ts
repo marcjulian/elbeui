a17cddf41af3b01c85fd74bb1e3ec66676720c3d
Marc Stammerjohann <8985933+marcjulian@users.noreply.github.com>
2025-12-15 18:13:36 +0100

    fix animate enter w/ initial values, add opacity-0 on animate.enter to apply initial w/o flash

--- libs/motion/src/lib/animate.ts
+++ libs/motion/src/lib/animate.ts
@@ -1,13 +1,11 @@
 import {
   afterNextRender,
   AnimationCallbackEvent,
+  computed,
   Directive,
-  effect,
   ElementRef,
   inject,
-  Injector,
   input,
-  signal,
   type OnDestroy,
 } from '@angular/core';
 import {
@@ -21,42 +19,47 @@ import {
   selector: '[animate]',
   exportAs: 'animate',
   host: {
-    '(animate.enter)': 'enteringFn($event)',
+    '[animate.enter]': '_animateEnterClass()',
     '(animate.leave)': 'leavingFn($event)',
   },
 })
 export class Animate implements OnDestroy {
   private readonly _element = inject(ElementRef);
-  private readonly _injector = inject(Injector);
 
   public readonly initial = input<DOMKeyframesDefinition>();
   public readonly animate = input.required<DOMKeyframesDefinition>();
   public readonly exit = input<DOMKeyframesDefinition>();
 
+  // hide element initially if initial animation is defined
+  protected readonly _animateEnterClass = computed(() =>
+    this.initial() ? 'opacity-0' : undefined,
+  );
+
   public readonly transition = input<AnimationOptions>();
 
   private _animation?: AnimationPlaybackControlsWithThen;
 
-  private readonly _initialized = signal(false);
-
   constructor() {
     afterNextRender(() => {
-      effect(
-        () => {
-          if (this._initialized()) {
-            // FIXME - sometimes the animate state is shortly visibile before switching to initial state
-            this._animation = animate(
-              this._element.nativeElement,
-              this.animate(),
-              this.transition(),
-            );
-          }
-        },
-        {
-          injector: this._injector,
-        },
-      );
+      const initial = this.initial();
+      if (initial) {
+        this.playInitial(initial, () => this.playAnimate());
+      } else {
+        this.playAnimate();
+      }
     });
+  }
+
+  private playInitial(initial: DOMKeyframesDefinition, onComplete: () => void) {
+    this._animation = animate(this._element.nativeElement, initial, {
+      // instantly apply initial styles
+      duration: 0,
+      onComplete,
+    });
+  }
+
+  private playAnimate() {
+    this._animation = animate(this._element.nativeElement, this.animate(), this.transition());
   }
 
   ngOnDestroy(): void {
@@ -65,22 +68,6 @@ export class Animate implements OnDestroy {
 
   replay() {
     this._animation?.play();
-  }
-
-  protected enteringFn(event: AnimationCallbackEvent) {
-    const initial = this.initial();
-    if (initial) {
-      animate(event.target, initial, {
-        duration: 0,
-        onComplete: () => {
-          event.animationComplete();
-          this._initialized.set(true);
-        },
-      });
-    } else {
-      event.animationComplete();
-      this._initialized.set(true);
-    }
   }
 
   protected leavingFn(event: AnimationCallbackEvent) {
